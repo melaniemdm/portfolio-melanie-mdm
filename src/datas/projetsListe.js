f967da1ee1cece34b4a617f49525b5ed8722cd38
melaniemdm <melanieborja.pro@gmail.com>
2022-04-25 15:36:54 +0200

    create dossier Herder et footer ffix datas

---
+++ src/datas/projetsListe.js
@@ -0,0 +1,17 @@
+export const logements = [
+	{
+		"id": "Projet N°1",
+		"title": "Reservia",
+		"picture": "assets/reservia2.jpg",
+        "describe":"Site de reservation d'hôtels",
+		"lien": "https://melaniemdm.github.io/Projet_2_Reservia/Reservia.html"
+		
+	},{
+    "id": "Projet N°2",
+    "title": "OhMyFood",
+    "picture": "assets/ohmyfood.jpg",
+    "describe":"Site pour mobile qui repertorie les menus de plusieurs retaurants gastronomiques",
+    "lien": "https://melaniemdm.github.io/Projet_3_ohmyfood/"
+
+}
+]
