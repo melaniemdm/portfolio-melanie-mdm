8ddb2cd7f34a20124d55db16f6b612a8033889f3
melaniemdm <melanieborja.pro@gmail.com>
2022-04-26 14:41:40 +0200

    feature(js et css) menu burger

--- src/layout/Navigation/index.js
+++ src/layout/Navigation/index.js
@@ -3,15 +3,22 @@ import "../../layout/Navigation/style.scss"
 function Navigation(){
 
     return (
-        <div >
- <nav className="navigate">
+        <div> 
+ <nav >
+ <label for="toggle">☰</label>
+    <input type="checkbox" id="toggle"/>
+  <div className="navigate">
     <a href="/home " className="home">Accueil</a> 
     <a href="/presentation" className="quisuisje">Qui suis-je?</a>
     <a href="/projets" className="projets">Mes Projets</a>
-    <a href="https://www.linkedin.com/in/m%C3%A9lanie-mariani-3275248a/" className="mail">Prendre Contact</a>
-    </nav>
-            
-        </div>
+    <a href="https://www.linkedin.com/in/m%C3%A9lanie-mariani-3275248a/" className="mail">Prendre Contact</a></div>
+  </nav>
+      
+       
+         </div>   
+
+
+ 
     )
 }
 export default Navigation
