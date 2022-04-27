ef30210ed6e0f3665477db61146a1891f09644e2
melaniemdm <melanieborja.pro@gmail.com>
2022-04-27 11:38:56 +0200

    fix(css & js) page de présentation

--- src/components/Presentation/index.js
+++ src/components/Presentation/index.js
@@ -1,10 +1,154 @@
+import '../../components/Presentation/style.scss';
+
 function PresentMe(){
     return (
-        <div>
+        <div  className="containerPrentation">
+<h2>Qui suis je?</h2>
 
-page de presentation
+<div className="txtPresentation">Après un début de carrière en tant qu'assistante de direction, j'ai découvert le Dev et c'est devenu une réelle passion.<br />Depuis maintenant 2 ans, je fais une formation de développement JavaScript - React (anciennement front-end).  <br /> Je compte poursuivre ensuite par un cursus développeur web de préférence en alternance afin d'augmenter mes connaissances et compétences. <br /> Je suis diponible pour de la freelance : https://www.malt.fr/profile/melanieborja<br />Vous pouvez également retrouver mes réalisations sur le lien suivant https://melaniemdm.github.io/
+
+</div>
+
+<h2>Mes compétences Professionelles</h2>
+
+<div className="containerListCompetence">
 
 
+<div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">Serieuse</p>
+        </div>
+    </div>
+    <div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">Investie</p>
+        </div>
+    </div>
+    <div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">Percéverante</p>
+        </div>
+    </div>
+    
+    
+    <div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">Sais me remettre en question</p>
+        </div>
+    </div>
+    <div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">JavaScrpit</p>
+        </div>
+    </div>
+    <div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">React</p>
+        </div>
+    </div>
+    <div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">UIKIT</p>
+        </div>
+    </div>
+
+    <div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">Tailwind CSS</p>
+        </div>
+    </div>
+
+    <div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">Bootrap</p>
+        </div>
+    </div>
+
+    <div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">Jest</p>
+        </div>
+    </div>
+
+    <div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">Jasmine</p>
+        </div>
+    </div>
+
+    <div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">Figma</p>
+        </div>
+    </div>
+
+    <div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">Simplébo</p>
+        </div>
+    </div>
+    <div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">Wordpress</p>
+        </div>
+    </div>
+    <div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">SASS</p>
+        </div>
+    </div>
+    <div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">JavaScript</p>
+        </div>
+    </div>
+    <div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">Réact</p>
+        </div>
+    </div>
+    <div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">Organisée</p>
+        </div>
+    </div>
+</div>
+
+<h2>Mes compétences défauts</h2>
+
+<div className="containerListCompetence">
+
+
+<div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">Méticuleuse : ce qui peut me prendre du temps malgrès tout je respect toujours les délais imposés</p>
+        </div>
+    </div>
+
+    <div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">impatiente : cette impatience me pousse à ne pas vivre sur mes acquis et à aller vite afin de respecter les délais, les contraintes.</p>
+        </div>
+    </div>
+
+    <div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">Timide : cela ne m'empeche d'aller à la rencontre de mes collegues/clients</p>
+        </div>
+    </div>
+    <div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">Obstinée : je ne baisse jamais les bras!</p>
+        </div>
+    </div>
+    <div className="uk-animation-toggle" tabIndex="0">
+        <div className="uk-card uk-card-default uk-card-body uk-animation-fade">
+            <p className="uk-text-center">Pour le reste je vous propose de faire connaissance !</p>
+        </div>
+    </div>
+
+
+</div>
         </div>
     )
 }
