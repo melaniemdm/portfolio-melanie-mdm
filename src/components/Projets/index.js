de161cf61a42bb5ee638e88f752672bb655deafa
melaniemdm <melanieborja.pro@gmail.com>
2022-04-27 16:38:34 +0200

    feature(css & js) card des projets

--- src/components/Projets/index.js
+++ src/components/Projets/index.js
@@ -1,10 +1,26 @@
+import '../../components/Projets/style.scss';
+import canard from '../../assets/fisheye.png';
+
 function Projets() {
 
     return (
-        <div>
-     
+        <div className="containerListCardProjet">
+     <div className="card">
+       <div className="titleProjet"> Réservia </div>
+       <div className="describe"> blavbpodujjpfuzsrfg </div>
+       </div>
+       <div className="ContainerImgProjet">
+       <a href="https://melaniemdm.github.io/fisheye/">
+       <img
+            className="imgProjet"
+                    src={canard}
+            alt="img du projet"
+          ></img>
+</a>
+       </div>
+     </div>
       
-      </div>
+      
     );
   }
   export default Projets;
